7c3c03d738a577e738ac2c947fe4a3d11b8aed87
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2023-06-09 20:05:43 -0400

    beyond0lte: bootleg bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The bootlegOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond0lte.mk
+    $(LOCAL_DIR)/bootleg_beyond0lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond0lte-eng \
-    lineage_beyond0lte-user \
-    lineage_beyond0lte-userdebug
+    bootleg_beyond0lte-eng \
+    bootleg_beyond0lte-user \
+    bootleg_beyond0lte-userdebug
